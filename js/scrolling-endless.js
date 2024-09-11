6b3a84f2e6d810765c138d02af0bed835753dc6c
polarbearweb <polarbearprogrammist@gamil.com>
2024-09-11 07:57:57 +0500

    Added scrolling of discount on main page

---
+++ js/scrolling-endless.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const scrollContainer = window;
+	const scrollContent = document.querySelector(".main__block-discount-list");
+	const containerWidth = scrollContainer.innerWidth;
+	const contentWidth = scrollContent.scrollWidth;
+
+	let startPosition = containerWidth;
+
+	function cloneItems() {
+		const items = document.querySelectorAll(".main__block-discount-item");
+		items.forEach((item) => {
+			const clone = item.cloneNode(true);
+			scrollContent.appendChild(clone);
+		});
+	}
+
+	function animateScroll() {
+		function step() {
+			startPosition -= 8;
+			if (startPosition < -contentWidth / 2) {
+				startPosition = 0;
+			}
+			scrollContent.style.transform = `translate(${startPosition}px, -100%)`;
+			requestAnimationFrame(step);
+		}
+		step();
+	}
+
+	cloneItems();
+	animateScroll();
+});
