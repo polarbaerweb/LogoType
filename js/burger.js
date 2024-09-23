463d5100c0531db5607a65439a7937f47061052b
polarbearweb <polarbearprogrammist@gamil.com>
2024-09-23 19:33:07 +0500

    Fixed bugs related to suggestion

--- js/burger.js
+++ js/burger.js
@@ -1,37 +1,119 @@
 document.addEventListener("DOMContentLoaded", handleDomeLoaded);
 
 function handleDomeLoaded() {
-	const elements_classname = [".header__navigation", ".burger", "body"];
-	const burger = new Burger(elements_classname);
+	const elements_classname = {
+		header: [".header__navigation"],
+		suggestion: [".main__services-selects"],
+	};
 
+	const burger = new Burger(elements_classname);
 	burger.handleActivation();
 }
 
 class Burger {
 	constructor(classnames) {
 		this.classnames = classnames;
-		this.elements = [];
+		this.burger_for = undefined;
+		this.burgers = [];
+		this.elements = {};
 	}
 
 	handleActivation() {
-		const burger = document.querySelector(".burger");
-		burger.onclick = () => this._activateElements();
-	}
+		const burger_container = document.querySelectorAll(".burger");
+		this.burgers = [...burger_container];
 
-	_activateElements() {
-		if (this.elements.length === 0) {
-			this._getAllElements();
-		}
+		burger_container.forEach((burger) => {
+			const burger_for = burger.dataset.burger_for;
 
-		this.elements.forEach((element) => {
-			element.classList.toggle("active");
+			burger.onclick = () => {
+				this._toggleBurger(burger, burger_for);
+			};
 		});
 	}
 
+	_removeActiveClassFromBurgers() {
+		this.burgers.forEach((burger) => {
+			burger.classList.remove("active");
+		});
+	}
+
+	_toggleBurger(burger, burger_for) {
+		const isActive = burger.classList.contains("active");
+
+		if (this.burger_for && this.burger_for !== burger_for) {
+			this._resetPreviousElements();
+			this._removeActiveClassFromBurgers();
+		}
+
+		burger.classList.toggle("active");
+
+		if (!isActive) {
+			this.burger_for = burger_for;
+		} else {
+			this.burger_for = undefined;
+		}
+
+		this._activateElements(burger_for);
+	}
+
+	_activateElements(burger_for) {
+		if (!this.elements[burger_for]) {
+			this._getAllElements();
+		}
+
+		this.elements[burger_for].forEach((element) => {
+			element.classList.toggle("active");
+		});
+
+		this._checkBurgerState();
+	}
+
+	_resetPreviousElements() {
+		if (this.burger_for) {
+			this.elements[this.burger_for].forEach((element) => {
+				element.classList.remove("active");
+			});
+		}
+
+		const anyActive = Object.keys(this.elements).some((key) =>
+			this.elements[key].some((element) =>
+				element.classList.contains("active"),
+			),
+		);
+
+		if (!anyActive) {
+			document.documentElement.classList.remove("active");
+		}
+	}
+
+	_checkBurgerState() {
+		const anyActive = this.elements[this.burger_for]?.some((element) =>
+			element.classList.contains("active"),
+		);
+
+		if (anyActive) {
+			document.documentElement.classList.add("active");
+		} else {
+			const hasOtherActive = Object.keys(this.elements).some((key) =>
+				this.elements[key].some((element) =>
+					element.classList.contains("active"),
+				),
+			);
+
+			if (!hasOtherActive) {
+				document.documentElement.classList.remove("active");
+			}
+		}
+	}
+
 	_getAllElements() {
-		this.classnames.forEach((classname) => {
-			let element = document.querySelector(classname);
-			this.elements.push(element);
+		this.elements[this.burger_for] = [];
+
+		this.classnames[this.burger_for].forEach((classname) => {
+			const element = document.querySelector(classname);
+			if (element) {
+				this.elements[this.burger_for].push(element);
+			}
 		});
 	}
 }
